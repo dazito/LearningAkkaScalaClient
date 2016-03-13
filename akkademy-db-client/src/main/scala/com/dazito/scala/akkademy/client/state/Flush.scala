e951a1a08d3e27059d96f20a21e07ae3945aaebb
Pedro <plm@dazito.com>
2016-03-13 23:48:01 +0000

    Finite State Machines - init

---
+++ akkademy-db-client/src/main/scala/com/dazito/scala/akkademy/client/state/Flush.scala
@@ -0,0 +1,8 @@
+package com.dazito.scala.akkademy.client.state
+
+/**
+ * Created by daz on 13/03/2016.
+ */
+case object Flush {
+
+}
---
+++ akkademy-db-client/src/main/scala/com/dazito/scala/akkademy/client/state/StateContainerTypes.scala
@@ -0,0 +1,10 @@
+package com.dazito.scala.akkademy.client.state
+
+import com.dazito.scala.dakkabase.messages.Request
+
+/**
+ * Created by daz on 13/03/2016.
+ */
+object StateContainerTypes {
+    type requestQueue = List[Request]
+}
